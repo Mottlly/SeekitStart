8d1041014ed8451ba51f6c186a0d1d325e6fd2e2
Mottlly <147268663+Mottlly@users.noreply.github.com>
2023-11-12 16:50:01 -0400

    playing around with JS

---
+++ index.js
@@ -0,0 +1,18 @@
+//things I need to make be able do
+// 1. Add food item to pantry
+// 2. have pantry update in dashboard
+// 3. have bad items display on dashboard
+// 4. have close to expiry items display on dashboard in order of ripeness
+// 5. have graphic update on dashboard
+
+
+let food_item = [
+    food_name='apple',
+    food_status='good',
+    food_type='F&V',
+    food_expiry='12/07/2024',
+    food_number='2',
+    food_owner='Tommy',
+    food_cost='1.26'
+
+]
