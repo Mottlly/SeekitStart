cc3277d929136cd8d6ee900be772870d69cc91a9
Mottlly <147268663+Mottlly@users.noreply.github.com>
2023-11-13 10:48:55 -0400

    oops,forgot to branch, my bad

--- index.js
+++ index.js
@@ -5,14 +5,10 @@
 // 4. have close to expiry items display on dashboard in order of ripeness
 // 5. have graphic update on dashboard
 
+function ButtonClick() {
+    alert("you clicked a button");
+}
 
-let food_item = [
-    food_name='apple',
-    food_status='good',
-    food_type='F&V',
-    food_expiry='12/07/2024',
-    food_number='2',
-    food_owner='Tommy',
-    food_cost='1.26'
+var button = document.getElementById("SubmitButton");
 
-]
+button.onclick = ButtonClick
