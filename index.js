37bf2076a11036f6239d1e767647eea82c78af39
Mottlly <147268663+Mottlly@users.noreply.github.com>
2024-03-17 15:46:11 -0300

    return to this tomorrow

--- index.js
+++ index.js
@@ -406,15 +406,18 @@ function ShowRecipes() {
   let foodvalue = document.getElementById("keyword");
   let keyword = foodvalue.value;
   let cuisinevalue = document.getElementById("cuisine");
-  let cuisine = cuisinevalue.value || "";
+  let cuisine = cuisinevalue.value || ;
   let mealtypevalue = document.getElementById("mealtype");
-  let mealtype = mealtypevalue.value || "";
-  console.log(mealtype);
+  let mealtype = mealtypevalue.value;
+  console.log(cuisine);
   if (keyword != "") {
     let call =
       "https://api.edamam.com/api/recipes/v2?type=public&q=" +
       keyword +
-      "&app_id=05567a42&app_key=b62303535d6536769387714d37717b61&random=true&field=label&field=image&field=source&field=url&field=ingredients&field=totalNutrients";
+      "&app_id=05567a42&app_key=b62303535d6536769387714d37717b61&cuisineType=" +
+      cuisine +
+      "&random=true&field=label&field=image&field=source&field=url&field=ingredients&field=totalNutrients";
+    console.log(call);
     xhttp.open("GET", call, true);
   }
   xhttp.send();
